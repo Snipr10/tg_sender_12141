3397ddb2a4b852ca8d19a78148b1addb7044fdb7
oleg.gusev <oleg.gusev@gmail.com>
2023-07-03 13:01:32 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+domain = "https://api.glassen-it.com/"
+auth_url = f"{domain}component/socparser/authorization/login"
+post_url = f"{domain}component/socparser/content/posts"
+
+login = "java_api"
+password = "4yEcwVnjEH7D"
+thread_id = 12141
+referenceFilter = []
+bot_data = "6336875684:AAG1ucDX4IwMNvCipUt-ocP82jHh__fBT-I"
+chart_id ="-908329096"
+UTC = -3
+FROM_MINUTES = 60
+POSTS_LIMIT = 5_000
